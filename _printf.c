bc32775ab4e9ce1b1068435df8363afe06a06f74
myUsername <myemail@gmail.com>
2023-04-23 10:38:58 -0700

    create -printf file and declare size and args variable

--- _printf.c
+++ _printf.c
@@ -6,11 +6,20 @@
 *
 *Returns: the number of characters printed.
 */
+
 int _printf(const char *format, ...)
+int _strlen(const char *)
+
 {
 	int size;
 	va_list args;
-
+if{
+	format == null;
+	return(-1);
+	}
+else{
+	size =_strelen(format);
+}
 
 
 	return (size);
