14c243a444198e2467f6eea1be08c09b80c8f77f
Mrscan20 <khalid.learning18@gmail.com>
2023-04-23 16:49:01 +0000

    create -printf file and declare size and args variable

---
+++ _printf.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* _printf - function that produces output according to a format.
+* @format: is a character string composed of zero or more directives.
+*
+*Returns: the number of characters printed.
+*/
+int _printf(const char *format, ...)
+{
+	int size;
+	va_list args;
+
+
+
+	return (size);
+}
