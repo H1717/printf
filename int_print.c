adc46836cfeb03b7c04df3d8027c60d7d76d2fe6
myUsername <myemail@gmail.com>
2023-04-24 10:16:55 -0700

    int_print

---
+++ int_print.c
@@ -0,0 +1,15 @@
+include "main.h"
+/**
+  *print_integer-print a number in base 10
+  *@list: number to print in base 10
+  *
+  *Return: lenght of the numbers in decimal
+  **/
+int print_integer(va_list list)
+{
+	char *ptr_b;
+	int size;
+	ptr_b = itoa(vaarg(list,int),10);
+	size = print((ptr_b != NULL) ? ptr_b : "NULL");
+	return(size);
+}
