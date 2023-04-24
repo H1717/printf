5eb9aac3d00e5a5c9acfa0b970c5d33498f613da
Mrscan20 <khalid.learning18@gmail.com>
2023-04-24 15:33:04 +0000

    update

--- handler.c
+++ handler.c
@@ -1,14 +1,16 @@
 #include "main.h"
+
 /**
-* handler - Format controller
-* @str: String format
-* @list: List of arguments
-*
-* Return: Total size of arguments with the total size of the base string
-**/
+ * handler - controller
+ * @str: String format
+ * @list: List of arguments
+ *
+ * Return: size of arguments + size of string
+ **/
 int handler(const char *str, va_list list)
 {
 	int size, i, x;
+
 	size = 0;
 	for (i = 0; str[i] != 0; i++)
 	{
@@ -17,11 +19,57 @@ int handler(const char *str, va_list list)
 			x = _handler(str, list, &i);
 			if (x == -1)
 				return (-1);
+
 			size += x;
 			continue;
 		}
+
 		_putchar(str[i]);
 		size = size + 1;
 	}
+
+
 	return (size);
+}
+
+/**
+ * _handler - Controller for percent format
+ * @str: String format
+ * @list: List of argument
+ * @i: iterator
+ *
+ * Return: Size of the numbers of elements printed
+ **/
+int _handler(const char *str, va_list list, int *i)
+{
+	int size, j, n;
+	format formats[] = {
+		{'s', str_print}, {'c', char_print},
+	};
+
+	*i = *i + 1;
+
+	if (str[*i] == '\0')
+		return (-1);
+
+	if (str[*i] == '%')
+	{
+		_putchar('%');
+		return (1);
+	}
+
+	n = sizeof(formats) / sizeof(formats[0]);
+	for (size = j = 0; j < n; j++)
+	{
+		if (str[*i] == formats[j].type)
+		{
+			size = formats[j].f(list);
+			return (size);
+		}
+
+	}
+
+	_putchar('%'), _putchar(str[*i]);
+
+	return (2);
 }
