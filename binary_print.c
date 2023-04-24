4d18dbcad5fa00092831014d3aa36ac047eadb90
myUsername <myemail@gmail.com>
2023-04-24 11:45:49 -0700

    binary_print

--- binary_print.c
+++ binary_print.c
@@ -1,4 +1,16 @@
 #include "main.h"
 
-/*
-
+/**
+  *binary_print - print and convert a number in base 2
+  *@list: Number to print
+  *
+  *Return:length
+  **/
+int binary_print(va_list list)
+{
+	char *ptr_b;
+	int size;
+	ptr_b = itoa(va_arg(list, unsigned int), 2);
+	size = print(ptr_b);
+return (size);
+}
