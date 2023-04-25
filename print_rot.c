3fb60f686d88ac1b369ac5bba6c71c9971e70869
myUsername <myemail@gmail.com>
2023-04-25 10:43:23 -0700

    print_rot

---
+++ print_rot.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_rot - Prints the rot13'ed string
+ * @list: String
+*
+ * Return: Length
+**/
+
+int print_rot(va_list list)
+{	
+char *p;
+int p_len;
+p = va_arg(list, char *);
+p_len = rot13((p != NULL) ? p : "(Wow)");
+	return (p_len);
+}
